0cdd25d5fac3a74de5d675465b25132b555ffecb
Edy Segura <edysegura@gmail.com>
2015-06-13 15:23:42 +0000

    Email sent

--- contact-form/send-email.php
+++ contact-form/send-email.php
@@ -5,10 +5,36 @@
 <meta charset="UTF-8">
 <title>Sent Data</title>
 
+<link rel="stylesheet" href="css/style.css" />
+
 </head>
 <body>
 
 <h1>Sent Data</h1>
+
+<p>
+    Name<br>
+    <?= $_POST['name'] ?>
+</p>
+
+<p>
+    Email<br>
+    <?= $_POST['email'] ?>
+</p>
+
+<p>
+    Message<br>
+    <pre><?= $_POST['message'] ?></pre>
+</p>
+
+<p>
+<?php 
+    $isSent = mail('edysegura@gmail.com', 'subject', 'message');
+    if($isSent) {
+        echo "Email sent!";
+    }
+?>
+</p>
 
 </body>
 </html>
